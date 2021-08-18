6e084ef2ef54b617faec2e677f61445515553b50
na7esan <na7esan@gmail.com>
2021-08-19 01:57:30 +0900

    add index view

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,4 +1,16 @@
-@php
-    dd($users);
-@endphp
-ユーザー一覧表示画面です
+@extends('layouts.app')
+
+@section('content')
+
+
+    <h1>会員一覧ページ</h1>
+
+    @foreach ($users as $user)
+
+        <p>名前:{{ $user->name }}</p>
+
+    @endforeach
+
+
+
+@endsection
