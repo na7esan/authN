d4a9bc826d25adb881228f07d97200030a192bcb
na7esan <na7esan@gmail.com>
2021-08-19 01:15:36 +0900

    add view register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,1 +1,28 @@
-会員登録のページです
+@extends('layouts.app')
+
+@section('content')
+
+
+    <h1>新規会員登録ページ</h1>
+
+    <form action="#" method="post">
+        @csrf
+
+        <label>お名前:
+            <input type="text" name="name">
+        </label>
+
+        <label>メールアドレス:
+            <input type="email" name="email">
+        </label>
+
+        <label>パスワード:
+            <input type="password" name="password">
+        </label>
+
+        <button type="submit">登録</button>
+    </form>
+
+
+
+@endsection
